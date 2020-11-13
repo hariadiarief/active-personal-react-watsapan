8925e14e5071b1ddb16c595fa3ead3ec2ab2571b
Masagus Hariadi Arief <adi.arief77@gmail.com>
2020-11-13 20:00:07 +0700

    redirect button and handle value input

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,12 @@ import PhoneInput from 'react-phone-input-2'
 
 export default function Home() {
 	const [number, setNumber] = useState(null)
+	const [isDisableToChat, setisDisableToChat] = useState(true)
+
+	function onSumbmit(event) {
+		event.preventDefault()
+		window.open(`https://wa.me/${number}`)
+	}
 
 	return (
 		<div className='container'>
@@ -15,16 +21,27 @@ export default function Home() {
 			<main className=''>
 				<span>Masukan No Whatsapp</span>
 
-				<PhoneInput
-					country={'id'}
-					defaultMask={'...-....-....'}
-					enableSearch={true}
-					disableSearchIcon={true}
-					countryCodeEditable={false}
-					value={number}
-					onChange={(phone) => setNumber(phone)}
-				/>
-				<button>Chat Now</button>
+				<form onSubmit={(event) => onSumbmit(event)}>
+					<PhoneInput
+						country={'id'}
+						defaultMask={'...-....-....'}
+						enableSearch={true}
+						disableSearchIcon={true}
+						countryCodeEditable={false}
+						value={number}
+						onChange={(phone) => {
+							setNumber(phone)
+							if (number?.length >= 11) {
+								setisDisableToChat(false)
+							} else {
+								setisDisableToChat(true)
+							}
+						}}
+					/>
+					<button disabled={isDisableToChat} type='submit'>
+						Chat Now
+					</button>
+				</form>
 			</main>
 
 			<footer className=''></footer>
